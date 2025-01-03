7d0b8f7aaf96d1cf998adf0956616f298355b02d
pawitchaya <boss@cmu.ac.th>
2025-01-03 22:18:52 +0700

    lab13_2.cpp completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,31 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void updateImage(bool image[][M], int s, int x, int y) {
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++) {
+           
+            int dx = i - x;
+            int dy = j - y;
+            if (sqrt(dx*dx + dy*dy) <= s - 1) {
+                image[i][j] = true;  
+            }
+        }
+    }
+}
+
+void showImage(const bool image[][M]) {
+    cout << "------------------------------------------------------------------------" << endl;
+    for(int i = 0; i < N; i++) {
+        cout << "|";
+        for(int j = 0; j < M; j++) {
+            if(image[i][j]) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << "|" << endl;
+    }
+    cout << "------------------------------------------------------------------------" << endl;
+}
